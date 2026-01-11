326c3cfcff14e7d8cbf1a836ddde7c018bb334c9
Disha Giri <dishagiri09170@gmail.com>
2026-01-11 11:39:19 +0530

    file download

---
+++ js/day 45/script.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector("button");
+const filler = document.getElementById("filler");
+let bar = 0;
+
+btn.addEventListener("click", function () {
+  if (bar == 0) {
+    filler.style.width = "100%";
+    btn.style.transform = "scale(1.05)";
+    bar = 1;
+    setTimeout(function () {
+      btn.innerText = "Downloaded";
+      btn.style.opacity = "0.5";
+      btn.style.transform = "scale(1)";
+      btn.disabled = true;
+      btn.style.cursor = "not-allowed";
+    }, 1000);
+  } else {
+    filler.style.width = "0%";
+    bar = 0;
+  }
+});
